bffdd3b95edc15e7bc80346f24efee0303908c54
Augusto dos Santos Pereira <augustogeog@gmail.com>
2021-06-09 23:06:49 -0300

    App first commti
    download shapefiles automatically in notebooks 0_collecting_data. Generates plotly mapbox figures in 1_test_territory and saves in json. Renders the figures in app.py.

---
+++ app/app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import plotly.io as pio
+
+
+@st.cache(suppress_st_warning=True)
+def load_metropolitan_areas(metro='Curitiba'):
+    """
+    Loads Plotly mapbox figures of selected metropolitan areas
+    """
+    dict_metro = {
+        'Manaus':'manaus'
+        , 'Belém':'belem'
+        , 'Fortaleza':'fortaleza'
+        , 'Recife':'recife'
+        , 'Salvador':'salvador'
+        , 'Belo Horizonte':'belo_horizonte'
+        , 'Vitória':'vitoria'
+        , 'Rio de Janeiro':'rio_de_janeiro'
+        , 'São Paulo':'sao_paulo'
+        , 'Campinas':'campinas'
+        , 'Curitiba':'curitiba'
+        , 'Florianópolis':'florianópolis'
+        , 'Porto Alegre':'porto_Alegre'
+        , 'Goiânia':'goiania'
+        , 'Brasília':'brasília'
+        }
+
+    fig = pio.read_json(f'../data/territory/json/plotly_fig/{dict_metro[metro]}.json')
+
+    return fig
+
+
+metropolitan_areas = (
+    'Manaus'
+    , 'Belém'
+    , 'Fortaleza'
+    , 'Recife'
+    , 'Salvador'
+    , 'Belo Horizonte'
+    , 'Vitória'
+    , 'Rio de Janeiro'
+    , 'São Paulo'
+    , 'Campinas'
+    , 'Curitiba'
+    , 'Florianópolis'
+    , 'Porto Alegre'
+    , 'Goiânia'
+    , 'Brasília'
+    )
+
+st.markdown(
+    f"<h1 style='text-align: left; color: black;'>Metropolitan Economic Trajectories</h1>", unsafe_allow_html=True
+)
+
+metro = st.selectbox(label='Metropolitan Area', options=metropolitan_areas)
+fig = load_metropolitan_areas(metro=metro)
+st.plotly_chart(fig)
