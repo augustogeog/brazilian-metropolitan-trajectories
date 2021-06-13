7953ab0e26a9766c778544fb11f144efeaf50b41
Augusto dos Santos Pereira <augustogeog@gmail.com>
2021-06-13 17:05:53 -0300

    use container width

--- app/app.py
+++ app/app.py
@@ -95,11 +95,11 @@ elif section == 'Population':
     if plot_type == 'Dynamic Bar':
         json_bar_pop = 'https://raw.githubusercontent.com/augustogeog/brazilian-metropolitan-trajectories/main/data/pop/px_bar_pop_70_18_dynamic.json'
         fig_bar_pop = load_plotly_fig(json_bar_pop)
-        st.plotly_chart(fig_bar_pop, height=300, width=700)
+        st.plotly_chart(fig_bar_pop,use_container_width=True, height=300, width=700)
     else:
         json_line_pop = 'https://raw.githubusercontent.com/augustogeog/brazilian-metropolitan-trajectories/main/data/pop/line_pop_70_18.json' 
         fig_line_pop = load_plotly_fig(json_line_pop)
-        st.plotly_chart(fig_line_pop, height=300, width=600)
+        st.plotly_chart(fig_line_pop,use_container_width=True, height=300, width=600)
 
 
 else:
